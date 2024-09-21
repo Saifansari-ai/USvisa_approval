5ebcc7eb423c1e3d126bb8ad7399c62fe6015ef1
Saifansari-ai <saifansari300b@gmail.com>
2024-09-21 08:48:52 +0530

    install dependies

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="saif",
+    author_email="saifansari300b@gmail.com",
+    packages=find_packages()
+)
